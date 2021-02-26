1ce2e70862c7054e3a10e07ecb48ec758fa694e2
Gianluigi Spisto <luigi.spisto92@gmail.com>
2021-02-26 18:29:39 +0100

    fixata var sconto 20,40%

--- js/script.js
+++ js/script.js
@@ -7,19 +7,24 @@ var etaUtente = prompt('Quanti anni hai?');
 //Prezzo per km
 var prezzoPerKm = 0.21;
 
+
 //-20% per i minorenni
 if(etaUtente < 18){
-    prezzoPerKm = 0.21 - 0.042;
-    alert('Grazie per l acquisto! Hai risparmiato il 20% grazie ad un buono sconto eta!');
+    prezzoPerKm = prezzoPerKm -((prezzoPerKm / 100) * 20);
+    document.writeln('Grazie per l acquisto! Hai risparmiato il 20% grazie ad un buono sconto eta!');
 
-    //-40% per gli over 65
+//-40% per gli over 65
 } else if (etaUtente > 65){
-    prezzoPerKm = 0.21 - 0.084;
-    alert('Grazie per l acquisto! Hai risparmiato il 40% grazie ad un buono sconto eta!');
+    prezzoPerKm = prezzoPerKm -((prezzoPerKm / 100) * 40);
+    document.writeln('Grazie per l acquisto! Hai risparmiato il 40% grazie ad un buono sconto eta!');
+}else{
+    document.writeln('Grazie per aver acquistato su Boolean Transport!');
 }
+
 
 
 //prezzo finale
 var totale = kmViaggio * prezzoPerKm;
+totale = totale.toFixed(2);
 
-alert('Il tuo biglietto costa ' + totale + '€');
+document.writeln('Il tuo biglietto costa ' + totale + '€');
